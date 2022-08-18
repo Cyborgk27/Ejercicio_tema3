5d1087b016d7105a4bcc343818cffb9eaadb3612
Cyborgk27 <slenderstalin@hotmail.com>
2022-08-17 23:57:51 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        String[] nombres ={"Kevin1","Kevin2","Kevin2","Kevin2","Kevin2","Kevin2","Kevin2","Kevin2","Kevin2","Kevin2","Kevin2","Kevin2","Kevin2"};
+        
+        for(String x: nombres){
+            System.out.print(x);
+        }
+    }
+}
